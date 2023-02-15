a4e43fbe13a14b56c8bb82cf9905ebd79b8012f6
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-15 11:23:06 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,57 @@
+export const item = {
+    "id": "771d9f31-8a41-4742-af80-8aeb5f9ad7fe",
+    "stories": [
+        {
+            "id": "5845f9c3-c087-48d2-a338-f7dece007d20",
+            "story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/89.png",
+            "story_label": "produce"
+        },
+        {
+            "id": "725f081f-cb97-4147-9644-d3f23bd46569",
+            "story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png",
+            "story_label": "treatment"
+        },
+        {
+            "id": "8d4f233f-e7c5-4be3-8af1-a241f2e4c582",
+            "story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/332.png",
+            "story_label": "young"
+        },
+        {
+            "id": "2e42d696-c7d8-4ec8-88aa-9aa0bfe8d3b9",
+            "story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png",
+            "story_label": "occur"
+        },
+        {
+            "id": "d836044e-842d-490c-b9fe-d4e4ffd4b043",
+            "story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png",
+            "story_label": "bag"
+        }
+    ],
+    "posts": [
+        {
+            "id": "622370ec-8046-4d6d-95ab-e512ece50559",
+            "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png",
+            "user_name": "simple"
+        },
+        {
+            "id": "b49a9d06-9e8f-4f16-a4d2-db98ca99382a",
+            "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/304.png",
+            "user_name": "money"
+        },
+        {
+            "id": "b424f52b-19db-4cc3-9e5b-580ce68b526e",
+            "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png",
+            "user_name": "sit"
+        },
+        {
+            "id": "78bf994c-69a0-49eb-aa71-9044618bf9c8",
+            "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png",
+            "user_name": "management"
+        },
+        {
+            "id": "3a3d1372-dd23-4faa-b033-246226671301",
+            "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png",
+            "user_name": "east"
+        }
+    ]
+}
